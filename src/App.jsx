511e34d3877747597fa7a4a8b8e3bcefce42c52c
matheus-ferreira1 <matheus.fer33@gmail.com>
2023-01-08 20:11:43 -0300

    added const with posts content

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,49 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/99987902?v=4",
+      name: "Matheus Ferreira",
+      role: "Web Developer",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+      },
+      {
+        type: "paragraph",
+        content: "elit, sed do eiusmod tempor incididunt ut",
+      },
+      { type: "link", content: "google.com" },
+    ],
+    publishedAt: new Date("2023-01-08 19:19:19"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/99987902?v=4",
+      name: "Raybih",
+      role: "Doctor",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing",
+      },
+      {
+        type: "paragraph",
+        content: "elit, sed do eiusmod tempor incididunt ut",
+      },
+      { type: "link", content: "yahoo.com" },
+    ],
+    publishedAt: new Date("2023-01-01 07:30:19"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -14,8 +57,13 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => (
+            <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,37 +1,49 @@
+import { format, formatDistanceToNow } from "date-fns";
+
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
+
 import styles from "./Post.module.css";
 
-export const Post = () => {
+export const Post = ({ author, publishedAt, content }) => {
+  const publishedDateFormatted = format(publishedAt, "LLLL d 'at' HH:mm'h'");
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/99987902?v=4" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Matheus Ferreira</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time dateTime="2023-01-07 22:03:04" title="January 7 2023">
-          Posted 1h ago
+        <time
+          dateTime={publishedDateFormatted}
+          title={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus velit
-        </p>
-        <p>
-          assumenda necessitatibus temporibus, hic natus quaerat quod ab
-          similique? Optio, delectus.
-        </p>
-        <p>
-          <a href="#">
-            ut rerum repellat iste veniam earum adipisci asperiores eligendi
-          </a>{" "}
-        </p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                {" "}
+                <a href="#">{line.content}</a>{" "}
+              </p>
+            );
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
